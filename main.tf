48576f94ab5ec80b65e64d2fcc7dc4894c766bfd
stefanopax <43370445+stefanopax@users.noreply.github.com>
2025-03-02 18:19:56 +0000

    Fix typo with variable interpolation

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "docker_network" "nextcloud_network" {
 # Plex container
 resource "docker_container" "plex" {
   name    = "plex"
-  image   = "linuxserver/plex:{var.plex_version}"
+  image   = "linuxserver/plex:${var.plex_version}"
   restart = "unless-stopped"
   env = [
     "TZ=Europe/Zurich",
@@ -44,7 +44,7 @@ resource "docker_container" "plex" {
 # qBittorrent container
 resource "docker_container" "qbittorrent" {
   name    = "qbittorrent"
-  image   = "linuxserver/qbittorrent:{var.qbittorrent_version}"
+  image   = "linuxserver/qbittorrent:${var.qbittorrent_version}"
   restart = "unless-stopped"
   env = [
     "PUID=1003",
@@ -66,7 +66,7 @@ resource "docker_container" "qbittorrent" {
 # Nextcloud container
 resource "docker_container" "nextcloud" {
   name    = "nextcloud"
-  image   = "linuxserver/nextcloud:{var.nextcloud_version}"
+  image   = "linuxserver/nextcloud:${var.nextcloud_version}"
   restart = "unless-stopped"
   env = [
     "PUID=999",
@@ -97,7 +97,7 @@ resource "docker_container" "nextcloud" {
 # Nextcloud Database container
 resource "docker_container" "nextclouddb" {
   name    = "nextclouddb"
-  image   = "mariadb:{var.mariadb_version}"
+  image   = "mariadb:${var.mariadb_version}"
   restart = "unless-stopped"
   env = [
     "PUID=999",
